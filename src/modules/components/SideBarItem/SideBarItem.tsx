fe688c43ce92c9a2024e68c13f318cec3959dc7d
oleksiikorotkov <ko.korotkovoleksii@gmai.com>
2023-12-27 19:42:34 +0200

    implement SideBarItem

---
+++ src/modules/components/SideBarItem/SideBarItem.tsx
@@ -0,0 +1,24 @@
+import { DisabledByDefault } from "styled-icons/material";
+
+interface ISideBarItemProp {
+  icon: any;
+  title: string;
+}
+
+const SideBarItem = ({
+  icon = <DisabledByDefault />,
+  title,
+}: ISideBarItemProp) => {
+  return (
+    <div className="flex pl-[16px] h-[40px] pt-2 pb-2 gap-[15px] hover:bg-[#EBE9E5] items-center">
+      <div className="w-[20px] h-[20px] flex justify-center items-center text-muted-foreground">
+        {icon}
+      </div>
+      <p className="color-[#1a1918] text-sm  select-none text-muted-foreground ">
+        {title}
+      </p>
+    </div>
+  );
+};
+
+export default SideBarItem;
