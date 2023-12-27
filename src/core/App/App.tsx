a4a939a4a24fb51be5796b1723a51cb1f27e5f66
oleksiikorotkov <ko.korotkovoleksii@gmai.com>
2023-12-27 13:36:46 +0200

    implement structure

---
+++ src/core/App/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/modules/components/ui/button";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <Button>button</Button>
+        <p>Hello</p>
+      </div>
+    </>
+  );
+};
+
+export default App;
