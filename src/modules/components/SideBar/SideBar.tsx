ade4c092c8c6316ccd5253df07f69c4ab8ae4a42
oleksiikorotkov <ko.korotkovoleksii@gmai.com>
2023-12-27 19:42:55 +0200

    add SideBar item

---
+++ src/modules/components/SideBar/SideBar.tsx
@@ -0,0 +1,34 @@
+import DropboxLogo from "@/assets/logo.svg";
+import { Files } from "styled-icons/bootstrap";
+import SideBarItem from "../SideBarItem/SideBarItem";
+import { Photo } from "styled-icons/material-outlined";
+import { Signature } from "styled-icons/fa-solid";
+import { PeopleOutline } from "styled-icons/material";
+import { Separator } from "@/modules/components/ui/separator";
+
+const sideItems = [
+  { icon: <Files />, title: "Усі файли" },
+  { icon: <Photo />, title: "Фотографії" },
+  { icon: <PeopleOutline />, title: "Спільний доступ" },
+  { icon: <Signature />, title: "Підписи" },
+];
+
+const SideBar = () => {
+  return (
+    <div className="w-[240px] h-[100vh] bg-[#F7F5F1] overflow-auto  border-r-[1px]">
+      <img
+        src={DropboxLogo}
+        alt="DropboxLogo"
+        className="w-7 h-7 ml-5 mt-[30px]"
+      />
+      <div className="mt-[30px]">
+        {sideItems.map((item, index) => (
+          <SideBarItem key={index} {...item} />
+        ))}
+      </div>
+      <Separator />
+    </div>
+  );
+};
+
+export default SideBar;
