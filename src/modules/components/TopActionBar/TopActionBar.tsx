3f2d57c98ff69e91a9c71d845e016b81b2fbeea8
oleksiikorotkov <ko.korotkovoleksii@gmai.com>
2023-12-27 23:13:58 +0200

    add simple tooltip

--- src/modules/components/TopActionBar/TopActionBar.tsx
+++ src/modules/components/TopActionBar/TopActionBar.tsx
@@ -3,6 +3,12 @@ import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { Bell, Grid3x3GapFill, QuestionCircle } from "styled-icons/bootstrap";
 import { Avatar, AvatarFallback } from "@/modules/components/ui/avatar";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/modules/components/ui/tooltip";
 
 const TopActionBar = () => {
   return (
@@ -15,13 +21,22 @@ const TopActionBar = () => {
         className=" w-[920px] h-[48px] rounded-none border-[1px] pl-[38px] text-base hover:border-current"
       />
       <div className="ml-4 flex flex-row items-center w-[140px]  justify-between">
-        <Button
-          variant={"ghost"}
-          size={"icon"}
-          className="w-8 h-8 rounded-none hover:bg-bgdropbox"
-        >
-          <Grid3x3GapFill className="h-4 w-4" />
-        </Button>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <Button
+                variant={"ghost"}
+                size={"icon"}
+                className="w-8 h-8 rounded-none hover:bg-bgdropbox"
+              >
+                <Grid3x3GapFill className="h-4 w-4" />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Ознайомитись з іншими продуктами Dropbox</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
         <Button
           variant={"ghost"}
           size="icon"
