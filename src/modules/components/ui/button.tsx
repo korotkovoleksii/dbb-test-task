6275e64cab08af642224bfc7afde1cf6b44b81e7
oleksiikorotkov <ko.korotkovoleksii@gmai.com>
2023-12-28 11:56:22 +0200

    add style for operation toolbar items

--- src/modules/components/ui/button.tsx
+++ src/modules/components/ui/button.tsx
@@ -18,12 +18,18 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        custom: "",
+        toolBarItemPrimary:
+          "bg-[#0160FE] text-[#fff] rounded-none text-[13px]   flex flex-col pl-2  gap-[5px] items-start cursor-auto hover:bg-[#004DC7]",
+        toolBarItemOrdinary:
+          "bg-[#fff] text-[#000] rounded-none text-[13px] border-[1px]  flex flex-col pl-2  gap-[5px] items-start cursor-auto  hover:bg-bgdropbox",
       },
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
         icon: "h-10 w-10",
+        toolbarItemSize: "w-[140px] h-[76px]",
       },
     },
     defaultVariants: {
