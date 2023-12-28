c8cb8e7b5f50aed19ab3da6ded511e7d294f0ccd
oleksiikorotkov <ko.korotkovoleksii@gmai.com>
2023-12-28 14:11:26 +0200

    refactoring

--- src/modules/components/TopActionBar/TopActionBar.tsx
+++ src/modules/components/TopActionBar/TopActionBar.tsx
@@ -9,8 +9,15 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/modules/components/ui/tooltip";
+import { useToast } from "../ui/use-toast";
 
 const TopActionBar = () => {
+  const { toast } = useToast();
+  const handleNotImplemented = () => {
+    toast({
+      title: "Not implemented ",
+    });
+  };
   return (
     <div className="relative flex flex-row items-center">
       <div className="w-[20px] absolute top-[50%] left-[25px] transform -translate-x-1/2 -translate-y-1/2">
@@ -28,6 +35,7 @@ const TopActionBar = () => {
                 variant={"ghost"}
                 size={"icon"}
                 className="w-8 h-8 rounded-none hover:bg-bgdropbox"
+                onClick={handleNotImplemented}
               >
                 <Grid3x3GapFill className="h-4 w-4" />
               </Button>
@@ -41,6 +49,7 @@ const TopActionBar = () => {
           variant={"ghost"}
           size="icon"
           className="w-8 h-8 rounded-none hover:bg-bgdropbox"
+          onClick={handleNotImplemented}
         >
           <QuestionCircle className="h-4 w-4" />
         </Button>
@@ -48,6 +57,7 @@ const TopActionBar = () => {
           variant={"ghost"}
           size="icon"
           className="w-8 h-8 rounded-none hover:bg-bgdropbox"
+          onClick={handleNotImplemented}
         >
           <Bell className="h-4 w-4" />
         </Button>
@@ -55,6 +65,7 @@ const TopActionBar = () => {
           variant={"ghost"}
           size="icon"
           className="w-8 h-8 rounded-none hover:bg-bgdropbox"
+          onClick={handleNotImplemented}
         >
           <Avatar className="size-6">
             <AvatarFallback>CN</AvatarFallback>
