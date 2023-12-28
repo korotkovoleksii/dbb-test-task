4414746533051bf589b4885ca0f1266c20d4d173
oleksiikorotkov <ko.korotkovoleksii@gmai.com>
2023-12-28 12:13:57 +0200

    integrate toast

--- src/core/App/App.tsx
+++ src/core/App/App.tsx
@@ -1,7 +1,7 @@
 import OperationToolbar from "@/modules/components/OperationToolbar/OperationToolbar";
 import SideBar from "@/modules/components/SideBar/SideBar";
 import TopActionBar from "@/modules/components/TopActionBar/TopActionBar";
-
+import { Toaster } from "@/modules/components/ui/toaster";
 const App = () => {
   return (
     <div className="flex gap-10">
@@ -11,6 +11,7 @@ const App = () => {
         <OperationToolbar />
         <div className="w-10 h-10 bg-red-500"></div>
       </div>
+      <Toaster />
     </div>
   );
 };
--- src/modules/components/OperationToolbar/OperationToolbar.tsx
+++ src/modules/components/OperationToolbar/OperationToolbar.tsx
@@ -7,47 +7,81 @@ import {
   Record2,
 } from "styled-icons/bootstrap";
 import { Button } from "../ui/button";
-
-const dataItemToolbar = [
-  {
-    title: "Створити",
-    icon: <Plus />,
-    primary: true,
-  },
-  {
-    title: "Передати",
-    icon: <ArrowBarUp />,
-    primary: false,
-  },
-  {
-    title: "Створити папку",
-    icon: <FolderPlus />,
-    primary: false,
-  },
-  {
-    title: "Записати",
-    icon: <Record2 />,
-    primary: false,
-  },
-  {
-    title: "Редагувати PDF",
-    icon: <Pen />,
-    primary: false,
-  },
-  {
-    title: "Отримати підпис",
-    icon: <FileArrowDown />,
-    primary: false,
-  },
-];
+import { useToast } from "../ui/use-toast";
 
 const OperationToolbar = () => {
+  const { toast } = useToast();
+
+  const dataItemToolbar = [
+    {
+      title: "Створити",
+      icon: <Plus />,
+      primary: true,
+      onClick: () => {
+        toast({
+          title: "Not implemented ",
+        });
+      },
+    },
+    {
+      title: "Передати",
+      icon: <ArrowBarUp />,
+      primary: false,
+      onClick: () => {
+        toast({
+          title: "Not implemented ",
+        });
+      },
+    },
+    {
+      title: "Створити папку",
+      icon: <FolderPlus />,
+      primary: false,
+      onClick: () => {
+        toast({
+          title: "Not implemented ",
+        });
+      },
+    },
+    {
+      title: "Записати",
+      icon: <Record2 />,
+      primary: false,
+      onClick: () => {
+        toast({
+          title: "Not implemented ",
+        });
+      },
+    },
+    {
+      title: "Редагувати PDF",
+      icon: <Pen />,
+      primary: false,
+      onClick: () => {
+        toast({
+          title: "Not implemented ",
+        });
+      },
+    },
+    {
+      title: "Отримати підпис",
+      icon: <FileArrowDown />,
+      primary: false,
+      onClick: () => {
+        toast({
+          title: "Not implemented ",
+        });
+      },
+    },
+  ];
+
   return (
     <div className="mt-4 flex gap-[16px]">
       {dataItemToolbar.map((item) => (
         <Button
           variant={item.primary ? "toolBarItemPrimary" : "toolBarItemOrdinary"}
           size={"toolbarItemSize"}
+          onClick={item.onClick}
         >
           <div className="h-5 w-5">{item.icon}</div>
           <p className="max-w-[90%]  truncate ...">{item.title}</p>
